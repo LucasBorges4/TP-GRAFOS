8684d27b228841d5d110b06ffe88d92f556a0615
Eduardo Antunes dos Santos Vieira <eduardoantunes986@gmail.com>
2023-10-11 12:52:13 -0300

    Adiciona uma versão inicial básica do programa
    Inclui também instruções de instalação e uso no README.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # TP-GRAFOS
+
+Esse programa requer que a versão 3 do python esteja instalada, bem como o
+pacote NetworkX. Assumindo que o python esteja instalado:
+
+```sh
+# Para instalar o networkx:
+$ pip install networkx
+# Para executar o programa com um arquivo GraphML:
+$ python3 graph.py ./exemplo.graphml
+```
---
+++ graph.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+def main(args):
+    from sys import stderr, exit
+    import networkx as nx
+    try:
+        # Leitura do grafo em GraphML
+        graph = nx.read_graphml(args[0])
+        print(f"Grafo {args[0]} lido com sucesso!")
+    except nx.NetworkXError:
+        # Ocorreu algum erro (i.e. o arquivo não é válido)
+        print(f"Não foi possível ler o grafo {args[0]}...", file=stderr)
+        exit(1)
+
+    while True:
+        # Menu bem simples
+        print("== OPERAÇÕES ==")
+        print("1. Obter a ordem do grafo")
+        print("2. Obter o tamanho do grafo")
+        print("3. Determinar os vizinhos de um vértice")
+        print("4. Determinar o grau de um vértice")
+        print("5. Obter a sequência de graus do grafo")
+        print("6. Determinar a excentricidade de um vértice")
+        print("7. Determinar o raio do grafo")
+        print("8. Determinar o diâmetro do grafo")
+        print("9. Determinar o centro do grafo")
+        print("10. Determinar o centro do grafo")
+        print("11. Realizar busca em largura (BFS)")
+        print("12. Obter a distância e caminho mínimo entre dois vértices")
+        print("13. Calcular a centralidade de proximidade de um vértice")
+        print("14. Sair")
+
+        # Leitura da operação desejada
+        op = int(input("Digite o número da opção escolhida: "))
+        if op < 0 and op > 14:
+            print("Opção não reconhecida, tente novamente")
+            continue
+
+        # Realização da operação desejada
+        if op == 1:
+            ordem = graph.number_of_nodes()
+            print(f"Ordem do grafo: {ordem}")
+        elif op == 2:
+            tam = graph.number_of_edges()
+            print(f"Tamanho do grafo: {tam}")
+        elif op == 14:
+            print("Saindo...")
+            break
+        else:
+            # Por enquanto, todas as operações não implementadas caem aqui
+            print(f"Operação {op} ainda sem implementação...", file=stderr)
+        print()
+
+
+if __name__ == "__main__":
+    from sys import argv
+    main(argv[1:])
